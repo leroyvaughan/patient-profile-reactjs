520b01edfbc1504d887e944c4fcff2b395872633
Gary Vaughan Jr <garyv@ygeiagroup.com>
2019-11-16 01:10:41 -0800

    - fixing node.js side for production build

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ app.use(bodyParser.json());
 
 
 
-app.get('/', (req, res) => {
-    res.redirect("/api/patient/4342012");
-})
+// app.get('/', (req, res) => {
+//     res.redirect("/api/patients");
+// })
 
 
 app.use("/api", patientRouter);
@@ -42,14 +42,14 @@ app.use(helmet());
 
 
 
-//// Serve static files from the React app
-//app.use(express.static(path.join(__dirname, 'client/build')));
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-//// The "catchall" handler: for any request that doesn't
-//// match one above, send back React's index.html file.
-//app.get('*', (req, res) => {
-//    res.sendFile(path.join(__dirname + '/client/build/index.html'));
-//});
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 
 // start server on the specified port, binding host
